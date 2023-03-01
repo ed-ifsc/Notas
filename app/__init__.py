d66a5ddeb3dcb6bd1fc809de9cb68499ccd2df51
Edjandir Corrêa Costa <edjandir@MacBook-Air-de-Edjandir.local>
2023-03-01 18:27:32 -0300

    model and signup page

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -22,9 +23,3 @@ def create_app():
         db.create_all()
 
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
-
+from flask import request, render_template, url_for, redirect, flash
+from werkzeug.security import generate_password_hash
+from .models import User
+from app import db
 
 auth = Blueprint('auth', __name__)
 
@@ -13,7 +16,34 @@ def logout():
     return '<h1>Logout</h1>'
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return '<h1>Sign-up</h1>'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email já existe.', category='error')
+        elif len(email) < 4:
+            flash('Email deve ter mais de 3 caracteres.', category='error')
+        elif len(first_name) < 2:
+            flash('O nome deve ter mais de 1 caracter.', category='error')
+        elif password1 != password2:
+            flash('Senhas não conferem.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+
+            new_user = User(email=email, first_name=first_name,
+                            password=generate_password_hash(password1,
+                                                            method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Conta criada!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html")
 
